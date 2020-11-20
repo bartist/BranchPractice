f34cc162a78dc2c4748a18b626d196b8ef5a3077
Coenchnb <chn.buitink12@gmail.com>
2020-11-20 09:11:13 +0100

    lijst met de antwoorden van alle berekeningen

--- minicalculator.py
+++ minicalculator.py
@@ -1,2 +1,11 @@
-kwadratischeInvoer = int(input("Vul hier uw invoer in om te kwadrateren"))
-print(kwadratischeInvoer**2)
+Kwadraten = []
+
+while True:
+    kwadratischeInvoer = int(input("Vul hier uw invoer in om te kwadrateren"))
+    Gekwadrateerd = kwadratischeInvoer**2
+    Kwadraten.append(Gekwadrateerd)
+    print(Gekwadrateerd)
+    Geschiedenis = input("Type geschiedenis voor de oude antwoorden ")
+    
+    if Geschiedenis == "geschiedenis":
+        print(Kwadraten)
