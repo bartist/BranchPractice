1ddcceff28c064c1cc7fc423a90c1c4cf25d0d5f
Coenchnb <chn.buitink12@gmail.com>
2020-11-27 09:22:11 +0100

    Rekenmachine afgemaakt en kan nu alle berekeningen uitvoeren.

--- minicalculator.py
+++ minicalculator.py
@@ -1,12 +1,40 @@
 ### Python easy calculator ###
-Kwadraten = []
+Uitkomsten = []
 
 while True:
-    kwadratischeInvoer = int(input("Vul hier uw invoer in om te kwadrateren"))
-    Gekwadrateerd = kwadratischeInvoer**2
-    Kwadraten.append(Gekwadrateerd)
-    print(Gekwadrateerd)
-    Geschiedenis = input("Type geschiedenis voor de oude antwoorden ")
+    InvoerBerekening = input("Wat wilt u berekenen (optellen, aftrekken, delen, vermenigvuldigen of kwadrateren: ")
+
+    if InvoerBerekening == "optellen":
+        Getal1, Getal2 = input("Vul hier uw twee getallen in om bij elkaar op te tellen: ").split()
+        OptelBerekening = int(Getal1) + int(Getal2)
+        Uitkomsten.append(OptelBerekening)
+        print(OptelBerekening)
+
+    elif InvoerBerekening == "aftrekken":
+        Getal1, Getal2 = input("Vul hier uw twee getallen in om van elkaar af te trekken: ").split()
+        AftrekBerekening = int(Getal1) - int(Getal2)
+        Uitkomsten.append(AftrekBerekening)
+        print(AftrekBerekening)
+
+    elif InvoerBerekening == "delen":
+        Getal1, Getal2 = input("Vul hier uw twee getallen in met elkaar te delen: ").split()
+        DeelBerekening = int(Getal1) / int(Getal2)
+        Uitkomsten.append(DeelBerekening)
+        print(DeelBerekening)
+
+    elif InvoerBerekening == "vermenigvuldigen":
+        Getal1, Getal2 = input("Vul hier uw twee getallen in met te vermenigvuldigen: ").split()
+        VermenigvuldigBerekening = int(Getal1) * int(Getal2)
+        Uitkomsten.append(VermenigvuldigBerekening)
+        print(VermenigvuldigBerekening)
+
+    elif InvoerBerekening == "kwadrateren":
+        Getal1 = int(input("Vul hier uw getal te kwadrateren: "))
+        KwadraatBerekening = Getal1**2
+        Uitkomsten.append(KwadraatBerekening)
+        print(KwadraatBerekening)
+
+    Geschiedenis = input("Type eventueel geschiedenis voor de oude antwoorden: ")
     
     if Geschiedenis == "geschiedenis":
-        print(Kwadraten)
+        print(Uitkomsten)
